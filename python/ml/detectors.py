47c0ae52c9a93415ba360f3110b46f7c19d6117f
feng <feng@edi-mac.local>
2026-05-30 22:00:41 +1000

    use box area when considering switch

--- python/ml/detectors.py
+++ python/ml/detectors.py
@@ -31,4 +31,26 @@ def annotate(image: Image.Image, result ) -> Image.Image:
 
 def analyze_result(res): 
     classes=res.boxes.cls
-    return (classes==0).sum().item(), (classes==3).sum().item(), (classes==4).sum().item()
+    def area(xyxy): 
+        c=xyxy.numpy()[0]
+        return abs((c[2]-c[0]) * (c[3]-c[1]))
+    xyxys=[box.xyxy for box in res.boxes if box.cls in [0,3,4]]
+    areas=[area(xyxy) for xyxy in xyxys]    
+    return (classes==0).sum().item(), (classes==3).sum().item(), (classes==4).sum().item(), sum(areas)
+
+if __name__ == "__main__":
+    import argparse
+    from common_utils import setup_logger
+    setup_logger('INFO')
+
+    imgs = [
+        "/Users/feng/workspace/runtime/tmp/aimpro/27595/left/3185-0.jpg",
+        "/Users/feng/workspace/runtime/tmp/aimpro/27595/left/3185-1.jpg",
+        "/Users/feng/workspace/runtime/tmp/aimpro/27595/left/3185-2.jpg"
+    ]
+    results = detect_objects_on_multiple_images(imgs)
+    for r in results:
+        # for box in r.boxes:
+        #     coordinates=box.xyxy.numpy()
+        #     logging.info(f"box: {coordinates}, shape = {coordinates.shape  }, class: {box.cls}, confidence: {box.conf}")
+        print(analyze_result(r))
